fb32049cdf92684c0705de0cda4c6562a436239c
Isaac Iniongun <iniongunisaac@gmail.com>
2019-09-13 21:37:55 +0100

    Create and Setup Project.

---
+++ app/src/main/java/com/fundall/ewallet/EWalletApplication.kt
@@ -0,0 +1,15 @@
+package com.fundall.ewallet
+
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+/**
+ * Created by Isaac Iniongun on 2019-09-13.
+ * For Fundall eWallet project.
+ */
+
+class EWalletApplication: DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
